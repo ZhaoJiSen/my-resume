c5c15fc708df99bf5c948317239e5295352229ae
zhaojisen <10357640+zhaojisen@user.noreply.gitee.com>
2024-02-02 13:25:07 +0800

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'my-resume',
-  integrations: [solidJs(), tailwind()]
+    base: 'my-resume',
+    integrations: [solidJs(), tailwind()],
 });
