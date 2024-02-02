63c7143523c17906e349ef163be5471430295ea5
zhaojisen <10357640+zhaojisen@user.noreply.gitee.com>
2024-02-02 13:40:38 +0800

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     base: 'my-resume',
+    publicDir: './public',
     integrations: [solidJs(), tailwind()],
 });
